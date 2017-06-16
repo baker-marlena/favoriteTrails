fab716c37422a0e23d3b8be97a68782d7ac64e30
Marlena Baker <baker.marlena@gmail.com>
2017-06-16 14:53:50 -0600

    connection set

--- knexfile.js
+++ knexfile.js
@@ -6,21 +6,10 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/trails'
   }
-  //
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+  }
 
 };
