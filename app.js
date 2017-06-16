aa2ab4012040fe66573f59de0943b41fa52a33e5
Marlena Baker <baker.marlena@gmail.com>
2017-06-16 13:05:02 -0600

    Adding knex files

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// const cors = require('cors');
 
 var app = express();
 
-var trails = require('./api/favoriteTrails')
+const trails = require('./api/trail');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -15,9 +16,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cors);
 
-app.use('/api/v1/favoriteTrails', favoriteTrails);
+app.use('/api/v1/trails', trails)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
